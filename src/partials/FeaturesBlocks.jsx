40f365e3b6bfe285ffee52cf981cdbd2ad835ab2
JustinMKY <64981306+JustinMKY@users.noreply.github.com>
2023-02-11 17:47:20 -0500

    changes

--- src/partials/FeaturesBlocks.jsx
+++ src/partials/FeaturesBlocks.jsx
@@ -13,8 +13,8 @@ function FeaturesBlocks() {
 
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-            <h2 className="h2 mb-4">How Simple works</h2>
-            <p className="text-xl text-gray-600">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur excepteur sint occaecat cupidatat.</p>
+            <h2 className="h2 mb-4">How it works</h2>
+            <p className="text-xl text-gray-600">Select a module and start typing!</p>
           </div>
 
           {/* Items */}
@@ -25,7 +25,7 @@ function FeaturesBlocks() {
               <svg className="w-16 h-16 p-1 -mt-1 mb-2" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
                 <g fill="none" fillRule="evenodd">
                   <rect className="fill-current text-blue-600" width="64" height="64" rx="32" />
-                  <g strokeWidth="2">
+                  <g strokeWidth="15">
                     <path className="stroke-current text-blue-300" d="M34.514 35.429l2.057 2.285h8M20.571 26.286h5.715l2.057 2.285" />
                     <path className="stroke-current text-white" d="M20.571 37.714h5.715L36.57 26.286h8" />
                     <path className="stroke-current text-blue-300" strokeLinecap="square" d="M41.143 34.286l3.428 3.428-3.428 3.429" />
