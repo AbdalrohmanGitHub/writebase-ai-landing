0c7eebe93ba7e0c31d8228541cdf5a97847775ce
JustinMKY <64981306+JustinMKY@users.noreply.github.com>
2023-02-11 18:48:37 -0500

    changes

--- src/partials/FeaturesBlocks.jsx
+++ src/partials/FeaturesBlocks.jsx
@@ -13,8 +13,8 @@ function FeaturesBlocks() {
 
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-            <h2 className="h2 mb-4">How it works</h2>
-            <p className="text-xl text-gray-600">Start generating content in 3 <span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-700 to-yellow-600">easy </span>steps</p>
+            <h2 className="h2 mb-4">How to use <span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-700 to-yellow-600">WriteBase AI</span></h2>
+            <p className="text-xl text-gray-600">Start generating content in 3 easy steps</p>
           </div>
 
           {/* Items */}
@@ -33,8 +33,8 @@ function FeaturesBlocks() {
                   </g>
                 </g>
               </svg>
-              <h4 className="text-xl font-bold leading-snug tracking-tight mb-1"><span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-700 to-yellow-600">1. </span>Select a module</h4>
-              <p className="text-gray-600 text-center">Choose from an array of AI tools to write social content, copy, articles & more.</p>
+              <h4 className="text-xl font-bold leading-snug tracking-tight mb-1">1. Select a module</h4>
+              <p className="text-gray-600 text-center">Choose from an array of AI tools to interpret code or write social content, copy, articles & more.</p>
             </div>
 
             {/* 2nd item */}
@@ -49,7 +49,7 @@ function FeaturesBlocks() {
                   </g>
                 </g>
               </svg>
-              <h4 className="text-xl font-bold leading-snug tracking-tight mb-1"><span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-700 to-yellow-600">2. </span>Fill in the details</h4>
+              <h4 className="text-xl font-bold leading-snug tracking-tight mb-1">2. Fill in the details</h4>
               <p className="text-gray-600 text-center">Explain with as many details as possible to the AI what you want the content to be about.</p>
             </div>
 
@@ -64,57 +64,8 @@ function FeaturesBlocks() {
                   </g>
                 </g>
               </svg>
-              <h4 className="text-xl font-bold leading-snug tracking-tight mb-1"><span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-700 to-yellow-600">3. </span>Generate AI content</h4>
+              <h4 className="text-xl font-bold leading-snug tracking-tight mb-1">3. Generate AI content</h4>
               <p className="text-gray-600 text-center">Our AI understands your details and generates unique human-like content in seconds.</p>
-            </div>
-
-            {/* 4th item */}
-            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl">
-              <svg className="w-16 h-16 p-1 -mt-1 mb-2" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
-                <g fill="none" fillRule="evenodd">
-                  <rect className="fill-current text-blue-600" width="64" height="64" rx="32" />
-                  <g transform="translate(22.857 19.429)" strokeWidth="2">
-                    <path className="stroke-current text-white" strokeLinecap="square" d="M12.571 4.571V0H0v25.143h12.571V20.57" />
-                    <path className="stroke-current text-white" d="M16 12.571h8" />
-                    <path className="stroke-current text-white" strokeLinecap="square" d="M19.429 8L24 12.571l-4.571 4.572" />
-                    <circle className="stroke-current text-blue-300" strokeLinecap="square" cx="12.571" cy="12.571" r="3.429" />
-                  </g>
-                </g>
-              </svg>              
-              <h4 className="text-xl font-bold leading-snug tracking-tight mb-1">Fast Prototyping</h4>
-              <p className="text-gray-600 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </div>
-
-            {/* 5th item */}
-            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl">
-              <svg className="w-16 h-16 p-1 -mt-1 mb-2" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
-                <g fill="none" fillRule="evenodd">
-                  <rect className="fill-current text-blue-600" width="64" height="64" rx="32" />
-                  <g strokeLinecap="square" strokeWidth="2">
-                    <path className="stroke-current text-white" d="M20.571 20.571h13.714v17.143H20.571z" />
-                    <path className="stroke-current text-blue-300" d="M38.858 26.993l6.397 1.73-4.473 16.549-13.24-3.58" />
-                  </g>
-                </g>
-              </svg>
-              <h4 className="text-xl font-bold leading-snug tracking-tight mb-1">Design Phase</h4>
-              <p className="text-gray-600 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </div>
-
-            {/* 6th item */}
-            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl">
-              <svg className="w-16 h-16 p-1 -mt-1 mb-2" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
-                <g fill="none" fillRule="evenodd">
-                  <rect className="fill-current text-blue-600" width="64" height="64" rx="32" />
-                  <g strokeWidth="2">
-                    <path className="stroke-current text-white" d="M32 37.714A5.714 5.714 0 0037.714 32a5.714 5.714 0 005.715 5.714" />
-                    <path className="stroke-current text-white" d="M32 37.714a5.714 5.714 0 015.714 5.715 5.714 5.714 0 015.715-5.715M20.571 26.286a5.714 5.714 0 005.715-5.715A5.714 5.714 0 0032 26.286" />
-                    <path className="stroke-current text-white" d="M20.571 26.286A5.714 5.714 0 0126.286 32 5.714 5.714 0 0132 26.286" />
-                    <path className="stroke-current text-blue-300" d="M21.714 40h4.572M24 37.714v4.572M37.714 24h4.572M40 21.714v4.572" strokeLinecap="square" />
-                  </g>
-                </g>
-              </svg>
-              <h4 className="text-xl font-bold leading-snug tracking-tight mb-1">Develop & Launch</h4>
-              <p className="text-gray-600 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
 
           </div>
