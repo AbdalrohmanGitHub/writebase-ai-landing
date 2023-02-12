89781a18cb8e88fad90152a588642503bffed5c2
JustinMKY <64981306+JustinMKY@users.noreply.github.com>
2023-02-11 19:01:11 -0500

    changes

--- src/partials/Footer.jsx
+++ src/partials/Footer.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Footer() {
+  const today = new Date();
+  const year = today.getFullYear();
   return (
     <footer>
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
@@ -11,7 +13,7 @@ function Footer() {
         <div className="md:flex md:items-center md:justify-between py-4 md:py-8 border-t border-gray-200">
 
           {/* Copyrights note */}
-          <div className="text-sm text-gray-600 mr-4">Made with 💖 by <a className="text-blue-600 hover:underline" href="#">WriteBase</a>. {<script>document.write(new Date().getFullYear())</script>} All rights reserved.</div>
+          <div className="text-sm text-gray-600 mr-4">Made with 💖 by <a className="text-blue-600 hover:underline" href="#">WriteBase</a>. <p>{year}</p> All rights reserved.</div>
 
         </div>
 
