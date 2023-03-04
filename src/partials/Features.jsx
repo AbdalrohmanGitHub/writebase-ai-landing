c799dbac70ed337c37a97398a8a447442ad6fc81
JustinMKY <64981306+JustinMKY@users.noreply.github.com>
2023-03-04 18:36:18 -0500

    change hero-images

--- src/partials/Features.jsx
+++ src/partials/Features.jsx
@@ -99,7 +99,7 @@ function Features() {
                 >
                   <div className="relative inline-flex flex-col">
                     <img className="md:max-w-none mx-auto rounded" src={FeaturesBg} width="500" height="462" alt="Features bg" />
-                    <img className="md:max-w-none absolute w-full left-0 transform animate-float" src={FeaturesElement} width="500" height="44" alt="Element" style={{ top: '30%' }} />
+                    {/* <img className="md:max-w-none absolute w-full left-0 transform animate-float" src={FeaturesElement} width="500" height="44" alt="Element" style={{ top: '30%' }} />*/}
                   </div>
                 </Transition>
                 {/* Item 2 */}
@@ -116,7 +116,7 @@ function Features() {
                 >
                   <div className="relative inline-flex flex-col">
                     <img className="md:max-w-none mx-auto rounded" src={FeaturesBg} width="500" height="462" alt="Features bg" />
-                    <img className="md:max-w-none absolute w-full left-0 transform animate-float" src={FeaturesElement} width="500" height="44" alt="Element" style={{ top: '30%' }} />
+                   {/*<img className="md:max-w-none absolute w-full left-0 transform animate-float" src={FeaturesElement} width="500" height="44" alt="Element" style={{ top: '30%' }} />*/}
                   </div>
                 </Transition>
                 {/* Item 3 */}
